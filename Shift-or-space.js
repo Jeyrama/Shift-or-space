732a5d3651ff51ea9aacecc130c00dcbb7fd2431
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-30 16:15:08 -0800

    Update Shift-or-space.js

--- Shift-or-space.js
+++ Shift-or-space.js
@@ -33,3 +33,32 @@ function getOrder(input) {
 }
 
 // or
+
+const getOrder = input => {
+  const menu = [
+    'burger',
+    'fries',
+    'chicken',
+    'pizza',
+    'sandwich',
+    'onionrings',
+    'milkshake',
+    'coke'
+  ];
+
+  let order = [];
+
+  while (input !== '') {
+    for (let i = 0; i < menu.length; i++) {
+      while (input.indexOf(menu[i]) !== -1) {
+        if (input.indexOf(menu[i]) >= 0) {
+          order.push(menu[i][0].toUpperCase() + menu[i].slice(1));
+          input = input.replace(menu[i], '');
+        }
+      }
+    }
+  }
+
+
+  return order.join(' ');
+};
